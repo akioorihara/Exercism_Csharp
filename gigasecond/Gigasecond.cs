e1ed3e874d061b4f03c80e323cdee30ee1d2cc1f
Akio Orihara <akio.orihara@outlook.com>
2021-07-22 15:44:57 -0500

    part 4 done

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -4,6 +4,7 @@ public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return moment.AddSeconds(1000000000);   
+        // throw new NotImplementedException("You need to implement this function.");
     }
 }
