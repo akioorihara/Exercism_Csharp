4f27b016ec98fbf9a44d9c45bea91179c3c35e92
Akio Orihara <akio.orihara@outlook.com>
2021-07-22 15:33:55 -0500

    part 3 done

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -4,6 +4,19 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(year % 4 == 0)
+        {
+                if (year % 100 == 0 && year % 400 == 0)
+                {
+                    return true;
+                }
+                else if (year % 100 == 0)
+                    return false;
+                else
+                    return true;
+        }
+            else
+                return false;
+        //throw new NotImplementedException("You need to implement this function.");
     }
 }
