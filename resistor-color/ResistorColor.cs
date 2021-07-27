3b79369b34d7125b38021edbf05db6c2eff3c8b3
Akio Orihara <akio.orihara@outlook.com>
2021-07-27 15:20:04 -0500

    resistorcolor is cone

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -4,11 +4,34 @@ public static class ResistorColor
 {
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        color.ToLower();
+        if (color == "black")
+            return 0;
+        else if (color == "brown")
+            return 1;
+        else if (color == "red")
+            return 2;
+        else if (color == "red")
+            return 3;
+        else if (color == "yellow")
+            return 4;
+        else if (color == "green")
+            return 5; 
+        else if (color == "blue")
+            return 6;
+        else if (color == "violet")
+            return 7;
+        else if (color == "grey")
+            return 8;
+        else
+            return 9; 
+        //throw new NotImplementedException("You need to implement this function.");
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string [] ColorString = new string [] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+        return ColorString;
+        //throw new notimplementedexception("you need to implement this function.");
     }
 }
