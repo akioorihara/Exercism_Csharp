eca3822c1feeebeedb026701bfc466051c5636bd
Akio Orihara <akio.orihara@outlook.com>
2021-08-11 13:09:37 -0500

    completed the clean up

--- high-scores/HighScores.cs
+++ high-scores/HighScores.cs
@@ -4,122 +4,73 @@ using System.Linq;
 
 public class HighScores
 {
-    List<int> list; 
-    //List<int> list = new List<int>(); 
-    //I thought this is how I have to do this to create an empty list 
-    
+    List<int> list;
+
+
     public HighScores(List<int> list)
     {
-        // var sut = new HighScores(new List<int> { 30, 50, 20, 70 });
-        // list.Sort();
-        // int max = 0, last = list.Count - 1;
-        // max = list[last];
-        // Console.WriteLine(max);
-        // // return max;
 
-        // List<int> listv2 = new List<int>(); 
-        this.list = list; 
+        this.list = list;
 
-
-        // throw new NotImplementedException();
     }
-
-//how do i know if there is void func? 
-//Score() returns the whole list but how should I return or print out?
 
     public List<int> Scores()
     {
-        // List<int> list = new List<int>(); 
-        // foreach(int i in list ){
-        //     Console.Write(i);
-        // }
+
         return list;
-        // throw new NotImplementedException();
     }
 
     public int Latest()
     {
-        // List<int> list = new List<int>(); 
-        int last = list.Count-1;
-        return list[last];
-        // throw new NotImplementedException();
+        //int last = list.Count - 1;
+        //return list[last];
+        return list.Last();
+
     }
-//This is not passing any args and how could I return or test agaist the list value? 
-
-
 
     public int PersonalBest()
     {
-
         return list.Max();
-        // throw new NotImplementedException();
     }
 
-
-
-   
     public List<int> PersonalTopThree()
     {
-
-        /*
+        List<int> cList = new List<int>(list);
+       
+        //cList = list; //If I use this, list also gets sorted and why does this also sort 'list' itself instead of just cList???
         List<int> v2 = new List<int>();
-        if (list.Count == 1){
-            list.Sort();
-            int l = list.Count-1; 
-            v2.Add(list[l]);
+        if (cList.Count == 1)
+        {
+            cList.Sort();
+            int l = cList.Count - 1;
+            v2.Add(cList[l]);
             return v2;
         }
-        else if (list.Count == 2)
+        else if (cList.Count == 2)
         {
-            list.Sort();
-            int l = list.Count-1; 
-            int v = list.Count-2;
-            v2.Add(list[l]);
-            v2.Add(list[v]);
-            return v2; 
+            cList.Sort();
+            int l = cList.Count - 1;
+            int v = cList.Count - 2;
+            v2.Add(cList[l]);
+            v2.Add(cList[v]);
+            return v2;
         }
 
-        int last = list.Count-1; 
-        int second = list.Count-2;
-        int  third = list.Count-3; 
-        // return list[last], list[second], list[third];
+        int last = list.Count - 1;
+        int second = list.Count - 2;
+        int third = list.Count - 3;
 
-        
-        int one = 0;
-        int two = 0;
-        int three = 0; 
-        int temp = 0;    
-        //Find Top Val 
-        foreach(int i in list)
-        {
-            if(Math.Max(i,one) == i)
-                one = i;
-        }
-
-        //Find the second top val 
-        foreach(int i in list)
-        {
-            if(one > i) { 
-                if(Math.Max(two, i) == i)
-                    two = i; 
-            }
-        }
-
-         
-        list.Sort();
-        last = list[last];
-        second = list[second];
-        third = list[third];
+        cList.Sort();
+        last = cList[last];
+        second = cList[second];
+        third = cList[third];
         v2.Add(last);
         v2.Add(second);
         v2.Add(third);
 
-        return v2; 
+        return v2;
 
-        */
-        //throw new NotImplementedException();
-       return list; 
-    } 
+    }
 }
 
 
