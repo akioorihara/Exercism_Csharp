fbf9438c1398af8306910f406c3f554a9bed34a5
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-08-13 23:26:11 -0500

    update and reading more docs

---
+++ robot-name/RobotName.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Robot
+{
+    public static string name;
+
+    public string Name
+    {
+        get
+        {
+            return Name; 
+            //throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
