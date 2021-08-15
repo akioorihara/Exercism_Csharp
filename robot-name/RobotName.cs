3ea323102c5783e3509b3551ae1dc2fc545de470
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-08-14 23:39:52 -0500

    just a few changes to it

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -2,10 +2,13 @@ using System;
 
 public class Robot
 {
-    public static string name;
+    //public static string name;
+    Random rand = new Random();
+    rand.Next(10); 
 
     public string Name
     {
+        //var rand = new Random();
         get
         {
             return Name; 
@@ -18,3 +21,6 @@ public class Robot
         throw new NotImplementedException("You need to implement this function.");
     }
 }
+
+
+//Assert.Matches(@"^[A-Z]{2}\d{3}$", robot.Name);
