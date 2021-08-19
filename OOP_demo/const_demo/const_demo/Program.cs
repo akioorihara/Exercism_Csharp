a59e34216a8589f3979005ba0da652fe3c39aa5a
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-08-19 07:59:04 -0500

    push this examples

---
+++ OOP_demo/const_demo/const_demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace const_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
