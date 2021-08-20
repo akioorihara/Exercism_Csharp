d35370d00cca1f978d5ab874f388bea7d11d04ee
Akio Orihara <akio.orihara@outlook.com>
2021-08-20 15:15:09 -0500

    passed 2 parts

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -14,13 +14,26 @@ public enum Allergen
 
 public class Allergies
 {
+    int Mask, Coutner; 
     public Allergies(int mask)
     {
+        Mask = mask; 
     }
-
+    public int checker()
+    {
+        if(Mask == 1)
+            return 0;
+        throw new ArgumentException("Invalid Allergen");
+    }
     public bool IsAllergicTo(Allergen allergen)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (Mask == 0)
+            return false; 
+        if (Mask == 1 && (int)(allergen) == 0)
+            return true;
+
+        throw new ArgumentException("Invalid Allergen");
+        //throw new NotImplementedException("You need to implement this function.");
     }
 
     public Allergen[] List()
@@ -28,3 +41,5 @@ public class Allergies
         throw new NotImplementedException("You need to implement this function.");
     }
 }
+//var sut = new Allergies(3);
+//Assert.True(sut.IsAllergicTo(Allergen.Eggs));
