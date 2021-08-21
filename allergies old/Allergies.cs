75f79adcc9d0c19f58d04e5fa1757848f6519ed7
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-08-20 23:20:41 -0500

    push

---
+++ allergies old/Allergies.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+
+public enum Allergen
+{
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
+}
+
+public class Allergies
+{
+    int Mask;
+    public List<string> list = new List<string>();
+
+    public Allergies(int mask)
+    {
+        Mask = mask; 
+    }
+   
+    public bool IsAllergicTo(Allergen allergen)
+    {
+        if (Mask <= 0)
+            return false;
+
+        while (Mask <= 0)
+        {
+            if (Mask >= (int)Allergen.Cats) {
+                Mask = Mask - (int)Allergen.Cats;
+                list.Add(allergen.ToString());
+            }
+
+            if (Mask >= (int)Allergen.Pollen)
+            {
+                Mask = Mask - (int)Allergen.Pollen;
+                list.Add(allergen.ToString());
+            }
+
+            if (Mask >= (int)Allergen.Chocolate)
+            {
+                Mask = Mask - (int)Allergen.Chocolate;
+                list.Add(allergen.ToString());
+            }
+
+            if (Mask >= (int)Allergen.Tomatoes)
+            {
+                Mask = Mask - (int)Allergen.Tomatoes;
+                list.Add(allergen.ToString());
+            }
+            if (Mask >= (int)Allergen.Strawberries)
+            {
+                Mask = Mask - (int)Allergen.Strawberries;
+                list.Add(allergen.ToString());
+            }
+            if (Mask >= (int)Allergen.Shellfish)
+            {
+                Mask = Mask - (int)Allergen.Shellfish;
+                list.Add(allergen.ToString());
+            }
+            if (Mask >= (int)Allergen.Peanuts)
+            {
+                Mask = Mask - (int)Allergen.Peanuts;
+                list.Add(allergen.ToString());
+            }
+            if (Mask >= (int)Allergen.Eggs)
+            {
+                Mask = Mask - (int)Allergen.Eggs;
+                list.Add(allergen.ToString());
+            }
+                   
+        }
+
+        //return (list.Contains(allergen.ToString())) ? true : false 
+        if (list.Contains(allergen.ToString()))
+            return true;
+        else
+            return false; 
+
+        throw new ArgumentException("Invalid Allergen");
+        //throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public Allergen[] List()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
+//var sut = new Allergies(0);
+//Assert.False(sut.IsAllergicTo(Allergen.Eggs));
+//var sut = new Allergies(3);
+//Assert.True(sut.IsAllergicTo(Allergen.Eggs));
+//var sut = new Allergies(1);
+//Assert.True(sut.IsAllergicTo(Allergen.Eggs));
--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -2,46 +2,25 @@ using System;
 
 public enum Allergen
 {
-    Eggs = 1 ,
-    Peanuts = 2,
-    Shellfish = 4,
-    Strawberries = 8,
-    Tomatoes = 16,
-    Chocolate = 32,
-    Pollen = 64,
-    Cats = 128
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats
 }
 
 public class Allergies
 {
-    int Mask, Coutner; 
     public Allergies(int mask)
     {
-        Mask = mask; 
     }
-    public int checker()
-    {
-        if(Mask == 1)
-            return 0;
-        throw new ArgumentException("Invalid Allergen");
-    }
+
     public bool IsAllergicTo(Allergen allergen)
     {
-        while(Mask != 0)
-        {
-            if(Mask > (int)allergen)
-            {
-                return true; 
-            }
-        }
-        if (Mask == 0)
-            return false; 
-        if (Mask == 1 && (int)(allergen) == 1)
-            return true;
-
-
-        throw new ArgumentException("Invalid Allergen");
-        //throw new NotImplementedException("You need to implement this function.");
+        throw new NotImplementedException("You need to implement this function.");
     }
 
     public Allergen[] List()
@@ -49,5 +28,3 @@ public class Allergies
         throw new NotImplementedException("You need to implement this function.");
     }
 }
-//var sut = new Allergies(3);
-//Assert.True(sut.IsAllergicTo(Allergen.Eggs));
