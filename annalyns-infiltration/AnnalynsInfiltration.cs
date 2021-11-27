cdf68d34b6b346d8f083ff0a171e6202536946d2
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-11-26 21:31:12 -0500

    Bool exercise is complete

--- annalyns-infiltration/AnnalynsInfiltration.cs
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -32,9 +32,11 @@ static class QuestLogic
     {
         //Signal prisoner: the prisoner can be signalled using bird sounds if the prisoner is awake and the archer is sleeping,
         //as archers are trained in bird signaling so they could intercept the message.
-        if (archerIsAwake is true && prisonerIsAwake is false)
+        //if (archerIsAwake is true && prisonerIsAwake is false)
+        
+        if(archerIsAwake is false && prisonerIsAwake is true)
         {
-            return true; 
+            return true;
         }
         else
         {
@@ -48,9 +50,35 @@ static class QuestLogic
         //Free prisoner: Annalyn can try sneaking into the camp to free the prisoner. This is a risky thing to do and can only succeed in one of two ways:
         //If Annalyn has her pet dog with her she can rescue the prisoner if the archer is asleep.
         //The knight is scared of the dog and the archer will not have time to get ready before Annalyn and the prisoner can escape.
+
         //If Annalyn does not have her dog then she and the prisoner must be very sneaky!
         //Annalyn can free the prisoner if the prisoner is awake and the knight and archer are both sleeping,
         //but if the prisoner is sleeping they can't be rescued: the prisoner would be startled by Annalyn's sudden appearance and wake up the knight and archer.
         //throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+
+
+        /*
+        Implement the (static) QuestLogic.CanFreePrisoner() method that takes four boolean values.
+        The first three parameters indicate if the knight, archer and the prisoner, respectively, are awake. The last parameter indicates if Annalyn's pet dog is present.
+        The method returns true if the prisoner can be freed based on the state of the three characters and Annalyn's pet dog presence. Otherwise, it returns false:
+         
+         */
+
+        if (petDogIsPresent is true && archerIsAwake is false){
+            return true; 
+        }
+        if (prisonerIsAwake is true && knightIsAwake is false && archerIsAwake is false)
+        {
+            if (prisonerIsAwake is true)
+            {
+                return true;
+            }
+            return false;
+        }
+        else;
+        {
+            return false; 
+        }
+
     }
 }
