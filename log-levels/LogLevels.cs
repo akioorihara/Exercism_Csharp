dbef3030abdb09024c82497fba00e6b02760f7ef
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-12-05 21:28:08 -0600

    the first part is done

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -4,18 +4,40 @@ static class LogLine
 {
     public static string Message(string logLine)
     {
-        if (logLine.Contains("[ERROR]:"))
+        string info_= "[INFO]:";
+        string waring_ = "[WARNING]:";
+        string error_ = "[ERROR]:";
+        string result = "";
+
+        if (logLine.Contains(error_))
         {
+            int i = logLine.IndexOf(error_);
+            result = logLine.Remove(i, error_.Length);
+            result = result.Trim();
+            return result; 
+        }
+        else if (logLine.Contains(info_))
+        {
+            int i = logLine.IndexOf(info_);
+            result = logLine.Remove(i, info_.Length);
+            result = result.Trim();
+            return result;
 
-            logLine.Replace("[ERROR]:", "");
-
-            return logLine;
+        }
+        else if (logLine.Contains(waring_))
+        {
+            int i = logLine.IndexOf(waring_);
+            result = logLine.Remove(i, waring_.Length);
+            result = result.Trim();
+            return result;
 
         }
         else
         {
+            //Console.WriteLine("Not Found");
             return "";
         }
+         
 
         //throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
     }
