097d9d38a9fd0f93a5d42289e0c21495593643a7
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-12-05 21:50:09 -0600

    passed all the tests

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -44,12 +44,79 @@ static class LogLine
 
     public static string LogLevel(string logLine)
     {
-        return logLine;
+        string toLow = logLine.ToLower();
+        //string result = "";
+        if (toLow.Contains("error"))
+        {
+            //result = toLow.IndexOf("\[");
+            //result = result.Trim();
+            return "error";
+        }
+        else if (toLow.Contains("info"))
+        {
+            return "info";
+
+        }
+        else if (toLow.Contains("warning"))
+        {
+            return "warning";
+
+        }
+        else
+        {
+            //Console.WriteLine("Not Found");
+            return "";
+        }
+
+
         //throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
     }
 
     public static string Reformat(string logLine)
     {
+        string info_ = "[INFO]:";
+        string waring_ = "[WARNING]:";
+        string error_ = "[ERROR]:";
+        string result = "";
+
+        if (logLine.Contains(error_))
+        {
+            int i = logLine.IndexOf(error_);
+            result = logLine.Remove(i, error_.Length);
+            result = result.Trim();
+            int len = result.Length;
+            result = result.Insert(len, " (error)");
+            return result;
+        }
+        else if (logLine.Contains(info_))
+        {
+            int i = logLine.IndexOf(info_);
+            result = logLine.Remove(i, info_.Length);
+            result = result.Trim();
+            int len = result.Length;
+            result = result.Insert(len, " (info)");
+            return result;
+
+        }
+        else if (logLine.Contains(waring_))
+        {
+            int i = logLine.IndexOf(waring_);
+            result = logLine.Remove(i, waring_.Length);
+            result = result.Trim();
+            int len = result.Length;
+            result = result.Insert(len, " (warning)");
+            return result;
+        }
+        else
+        {
+            //Console.WriteLine("Not Found");
+            return "";
+        }
+
+
+
+
+
         return logLine;
         //throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
     }
