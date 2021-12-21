4d4de91cd8aa527d6407ea1eaea8dbd81b0d8d30
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-12-20 21:47:48 -0600

    update

---
+++ phone-numer-analysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+        string x = "";
+        phoneNumber = "";
+        return (false, false, "");
+        //throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+    {
+        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+    }
+}
