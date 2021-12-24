338448e65b27190fb53531eb0c495e2f80a78955
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-12-24 15:42:21 -0600

    completed the tim-from-marketing

--- tim-from-marketing/TimFromMarketing.cs
+++ tim-from-marketing/TimFromMarketing.cs
@@ -4,6 +4,27 @@ static class Badge
 {
     public static string Print(int? id, string name, string? department)
     {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
+        
+        if (id != null && department != null)
+        {
+            return "[" + id + "] - " + name + " - " + department.ToUpper();
+        }
+        else if (id == null && department != null)
+        {
+            return name + " - " + department.ToUpper();
+        }
+        else if (department == null && id != null)
+        {
+            return "[" + id + "] - " + name + " - " + "OWNER";
+        }
+        else if (department == null && id == null)
+        {
+            return name + " - " + "OWNER";
+        }
+        else
+        {
+            return "";
+        }
+
     }
 }
