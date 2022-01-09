20cfb33ed0629b3cbe01112db312ca297b876562
akioorihara <56176776+akioorihara@users.noreply.github.com>
2022-01-09 09:44:59 -0600

    passed the tests

--- darts/Darts.cs
+++ darts/Darts.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
 public static class Darts
 {
     public static int Score(double x, double y)
     {
-
-
-        return 0;
-       //throw new NotImplementedException("You need to implement this function");
+        double distance = Distance(x, y);
+        if (distance <= 1)
+            return 10;
+        else if (distance <= 5)
+            return 5;
+        else if (distance <= 10)
+            return 1;
+        else
+            return 0;
+    }
+    private static double Distance(double x, double y)
+    {
+        return System.Math.Sqrt(x * x + y * y);
     }
 }
