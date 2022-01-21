5cdeb68511241b345914793ca212c01ee4b0b8f3
akioorihara <56176776+akioorihara@users.noreply.github.com>
2022-01-20 21:34:49 -0600

    no errors now

--- bob/Bob1.cs
+++ bob/Bob1.cs
@@ -1,60 +1,37 @@
 using System;
-
-public static class EntMethods
-{
-
-<<<<<<< HEAD
-    public static string Response(string statement)
-    {
-=======
-    /// <summary>
-    /// Remove all instances of ToRemove with empty srting
-    /// </summary>
-    /// <param name="In"></param>
-    /// <param name="ToRemove">The String to remove</param>
-    /// <returns></returns>
-    public static string RemoveIt(this string In, string ToRemove)
-    {
-        return In.Replace(ToRemove, "");
-    }
-}
-
+//public static class EntMethods
+//{
+//    /// <summary>
+//    /// Remove all instances of ToRemove with empty srting
+//    /// </summary>
+//    /// <param name="In"></param>
+//    /// <param name="ToRemove">The String to remove</param>
+//    /// <returns></returns>
+//    //public static string RemoveIt(this string In, string ToRemove)
+//    //{
+//    //    return In.Replace(ToRemove, "");
+//    //}
+//}
 public static class Bob
-{ 
-
+{
     public static string Response(string statement)
     {
         statement = statement.Trim();
->>>>>>> a16b768124f165a8bd00ade7b955c019036a4aa9
         statement = statement.Trim(' ');
         statement = statement.Trim('\t');
         statement = statement.Trim('\r');
         statement = statement.Trim('\n');
-<<<<<<< HEAD
-
-        statement = statement.Replace(" ", "");
-=======
         statement = statement.Replace(" ", "");
         statement = statement.Replace("\n", "");
-
-        statement = statement.RemoveIt(" ");
-
+        //statement = statement.RemoveIt(" ");
         if (statement.Trim().Length == 0)
         {
-
         }
-
         //statement = " "; // the diff from the above codes 
->>>>>>> a16b768124f165a8bd00ade7b955c019036a4aa9
-
         if (statement == "")
         {
             return "Fine. Be that way!";
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> a16b768124f165a8bd00ade7b955c019036a4aa9
         else if (isAllUpper(statement))
         {
             //yell a question 
@@ -62,58 +39,20 @@ public static class Bob
                 return "Calm down, I know what I'm doing!";
             return "Whoa, chill out!";
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> a16b768124f165a8bd00ade7b955c019036a4aa9
         //Question 
-        else if (isQuestion(statement))
+        else if (isQustion(statement))
         {
             return "Sure.";
         }
-<<<<<<< HEAD
-        
-=======
-
->>>>>>> a16b768124f165a8bd00ade7b955c019036a4aa9
         //Anything else 
         else
         {
             return "Whatever.";
         }
         //throw new NotImplementedException("You need to implement this function.");
-<<<<<<< HEAD
     }
-
-    public static bool isAllUpper(string statement)
-    {
-        for(int i = 0; i < statement.Length; i++)
-        {
-            if (!Char.IsUpper(statement[i]))
-
-             //I have to skip the special char and digit here 
-             //&& !char.IsSymbol(statement[i]) && !char.IsNumber(statement[i]) )
-            {
-                return false; 
-            }
-        }
-        return true;
-    }
-
-    public static bool isQuestion(string statement)
-    {
-        if (statement[statement.Length-1] == '?')
-            return true; 
-        return false; 
-    }
-
-}
-=======
-    }
-
     private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     //private const string Punctuation = "?!.;:";
-
     private static bool isAllUpper(string statement)
     {
         for (int i = 0; i < statement.Length; i++)
@@ -127,15 +66,15 @@ public static class Bob
                     return false;
                 }
             }
-
         }
         return true;
+
     }
-    private static bool isQuestion(string statement)
+    private static bool isQustion(string statement)
     {
-        if (statement[statement.Length - 1] == '?')
-            return true;
-        return false;
+
+        return false; 
     }
+
 }
->>>>>>> a16b768124f165a8bd00ade7b955c019036a4aa9
+   
