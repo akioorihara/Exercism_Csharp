2552297533981d2b26b0cac1ecd1cbf2b94b9784
Akio Orihara <akioorihara@protonmail.com>
2023-06-10 14:04:07 -0500

    diff of Squares completed

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -4,14 +4,25 @@ public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-
-        return 0;
+        var x = 0;
+        for(int i = 1; i <= max; i++)
+        {
+            x += i;
+        }
+        return x * x;
+        //var Sum = (max / 2) * (1 + max);
+        //var SquareOfSum = Convert.ToInt32(Math.Pow(Sum, 2));
+        //return SquareOfSum ;
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-
-        return 0;
+        int sumOfSquares = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            sumOfSquares += Convert.ToInt32(Math.Pow(i, 2)); 
+        }
+        return sumOfSquares;
     }
 
     public static int CalculateDifferenceOfSquares(int max)
