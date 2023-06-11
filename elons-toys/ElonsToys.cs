7807b684b5d1d3a58ae71fca4db9c66869c31936
Akio Orihara <akioorihara@protonmail.com>
2023-06-10 22:02:28 -0500

    refactor the codes and code clean up

--- elons-toys/ElonsToys.cs
+++ elons-toys/ElonsToys.cs
@@ -2,41 +2,47 @@ using System;
 
 class RemoteControlCar
 {
-    //20 meters and drains one percent of battries
-    //Start with 100$ of battries
+    private static readonly int _startingDistance = 0;
+    private static readonly int _startingBattery = 100;
 
-    public int battery = 100;
-    public int Driven = 0;
+    private int _remainingBattery = 100;
+    private int _distanceDriven = 0;
 
+    private int _milesDrivenPerDrive  = 20;
+    private int _batteryPerDrive = 1;
+
+    public RemoteControlCar()
+    {
+        _remainingBattery = _startingBattery;
+        _distanceDriven = _startingDistance;
+    }
 
     public static RemoteControlCar Buy()
     {
-
-        //The below is important 
         return new RemoteControlCar();
     }
 
 
     public string DistanceDisplay()
     {
-        return $"Driven {Driven} meters";
+        return $"Driven {_distanceDriven} meters";
     }
 
     public string BatteryDisplay()
     {
-        if (battery <= 0) //at 0%
+        if (_remainingBattery <= 0) //at 0%
         {
             return "Battery empty";
         }
-        return $"Battery at {battery}";
+        return $"Battery at {_remainingBattery}%";
     }
 
     public void Drive()
     {
-        if (battery > 0)
+        if (_remainingBattery > 0)
         {
-            Driven += 20;
-            battery -= 1;
+            _distanceDriven += _milesDrivenPerDrive;
+            _remainingBattery -= _batteryPerDrive;
         }
     }
 
