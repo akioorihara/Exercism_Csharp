705ae15a67ae41add6a7b0a52c2640beb4483207
Akio Orihara <akioorihara@protonmail.com>
2023-08-11 21:48:00 -0500

    clean up and finished this lesson

--- need-for-speed/NeedForSpeed.cs
+++ need-for-speed/NeedForSpeed.cs
@@ -39,13 +39,11 @@ class RemoteControlCar
             this.meterDriven += speed;
             this.battery -= batteryDrain; //-- ?? not sure 
         }
-        //TODO: check speed in meters & drain battery by BatteryDrained()
-
+        
     }
 
     public static RemoteControlCar Nitro()
     {
-        //TODO: How can I make it 4% - make it decimal? 
         return new RemoteControlCar(50, 4);
     }
 }
@@ -61,17 +59,18 @@ class RaceTrack
     public bool TryFinishTrack(RemoteControlCar car)
     {
 
-        //TODO: 
         decimal distanceBySpeed = (distance / (decimal)car.speed);
         var carFinish = car.batteryDrain * distanceBySpeed <= 100;
         return carFinish;
 
-        //var possibleDrain = distanceBySpeed * car.batteryDrain;
+        //var possibleDrain = distanceBySpeed * car.batteryDrain <= 100;
+        //return possibleDrain;
+
         //if (possibleDrain >= car.battery)
         //    return true;
 
         //return false;
-        
+
 
 
     }
