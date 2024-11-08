045a917f5c42f47ff84277bc6701a6c94ddedd45
Akio Orihara <akioorihara@protonmail.com>
2024-11-08 15:18:37 -0600

    adding more packages to this

---
+++ roman-niumerals/RomanNumerals.cs
@@ -0,0 +1,37 @@
+using System;
+
+public static class RomanNumeralExtension
+{
+
+    // This method is to convert arabic number to roman numeral
+    public static string ToRoman(this int value)
+    {
+        //if the value > 5, return with IIII 
+        if(value < 5){
+            if(value == 1){
+                return "I";
+            }
+            else if(value == 2){
+                return "II";
+            }
+            else if(value == 3){
+                return "IIII";
+            }
+            else{
+                return "IIII";
+            }
+        }
+        
+        // if one fo the values listed, convert and then return 
+
+
+
+        
+        //check if the prev value is smaller? 
+
+        //if so, get the value and then substract the value 
+        
+        
+        return "";
+    }
+}
