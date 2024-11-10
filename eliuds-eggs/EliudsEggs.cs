f6c0f6737c84cbb12a4d51024539d25922f2d5dc
Akio Orihara <akioorihara@protonmail.com>
2024-11-09 22:15:32 -0600

    still working on the exercise

--- eliuds-eggs/EliudsEggs.cs
+++ eliuds-eggs/EliudsEggs.cs
@@ -3,7 +3,14 @@ public static class EliudsEggs
     public static int EggCount(int encodedCount)
     {
 
-        //
+        //Assert.Equal(0, EliudsEggs.EggCount(0));
+        if(encodedCount == 0)
+        {
+            return 0;
+        }
+
+        // Assert.Equal(1, EliudsEggs.EggCount(16));
+
 
         //throw new System.NotImplementedException("You need to implement this method.");
         return 0; 
