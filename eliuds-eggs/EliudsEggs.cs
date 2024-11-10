e83436b26156636288615c1a205e735c3634310a
Akio Orihara <akioorihara@protonmail.com>
2024-11-09 22:04:37 -0600

    setting up and figuring out

---
+++ eliuds-eggs/EliudsEggs.cs
@@ -0,0 +1,11 @@
+public static class EliudsEggs
+{
+    public static int EggCount(int encodedCount)
+    {
+
+        //
+
+        //throw new System.NotImplementedException("You need to implement this method.");
+        return 0; 
+    }
+}
--- roman_numerals/RomanNumerals.cs
+++ roman_numerals/RomanNumerals.cs
@@ -6,6 +6,9 @@ public static class RomanNumeralExtension
     // This method is to convert arabic number to roman numeral
     public static string ToRoman(this int value)
     {
+        Console.WriteLine("hello world here!");
+
+
         //if the value > 5, return with IIII 
         if(value < 5){
             if(value == 1){
